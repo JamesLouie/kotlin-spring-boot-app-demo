6f7f4db307616b58ab245ca63314c8f442ebcbe5
James Louie <louie@Jamess-MacBook-Pro.local>
2020-05-08 17:24:23 -0700

    Demo for ConfigurationProperties

--- src/main/kotlin/com/jclouie/demo/DemoApplication.kt
+++ src/main/kotlin/com/jclouie/demo/DemoApplication.kt
@@ -1,9 +1,11 @@
 package com.jclouie.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class DemoApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/jclouie/demo/api/controllers/HelloWorldController.kt
+++ src/main/kotlin/com/jclouie/demo/api/controllers/HelloWorldController.kt
@@ -1,13 +1,14 @@
 package com.jclouie.demo.api.controllers
 
+import com.jclouie.demo.api.configuration.DatabaseConfig
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class HelloWorldController {
+class HelloWorldController(val databaseConfig: DatabaseConfig) {
 
     @GetMapping("/")
     fun hi(): String {
-        return "Hello World"
+        return "Hello World ${databaseConfig.username}"
     }
 }
